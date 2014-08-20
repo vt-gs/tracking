4bb63dd2409d48eccbf925b3c828bd007e5e67f9
Zach Leffke <zleffke@vt.edu>
2014-08-20 11:10:23 -0400

    added readline() capability and parsing functionality for feedback data

--- man_track/man_track.py
+++ man_track/man_track.py
@@ -5,26 +5,54 @@ import string
 import sys
 import time
 import curses
-from threading import Thread
+import threading
 from optparse import OptionParser
 
-class Tracker_Thread(Thread):
+class Tracker_Thread(threading.Thread):
     def __init__ (self, options):
-        Thread.__init__(self)
+        threading.Thread.__init__(self)
+        self._stop = threading.Event()
         self.ip = options.ip
         self.port = options.port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP Socket
+        self.cmd_az = 0.0       #Commanded Azimuth
+        self.cmd_el = 0.0       #Commanded Elevation
+        self.current_az = 0.0   #Current Azimuth
+        self.current_el = 0.0   #Current Elevation
+        self.az_tol = 0.0       #Azimuth Tolerance
+        self.el_tol = 0.0       #Elevation Tolerance
+        self.status = ""        #Antenna Pedestal Status
 
     def run(self):
         self.sock.connect((self.ip,self.port))
-        while 1:
-            data = self.sock.recv(1024)
-            print data
-            time.sleep(0.25)
+        while (not self._stop.isSet()):
+            #data = self.sock.recv(1024)
+            data = self.sock.makefile().readline(1024)
+            data = data.strip('\n')
+            data_list = data.split(',')
+            if data_list[0] == '$':
+                self.cmd_az     = float(data_list[1])   #Commanded Azimuth
+                self.cmd_el     = float(data_list[2])   #Commanded Elevation
+                self.current_az = float(data_list[3])   #Current Azimuth
+                self.current_el = float(data_list[4])   #Current Elevation
+                self.az_tol     = float(data_list[5])   #Azimuth Tolerance
+                self.el_tol     = float(data_list[6])   #Elevation Tolerance
+                self.status     = data_list[7].strip('\r')        #Antenna Pedestal Status
+            print self.cmd_az, self.cmd_el, self.current_az, self.current_el, self.az_tol, self.el_tol, self.status
+        sys.exit()
+            #time.sleep(0.25)
 
-class Display_Thread(Thread):
+    
+    
+    def stop(self):
+        self._stop.set()
+
+    def stopped(self):
+        return self._stop.isSet()
+
+class Display_Thread(threading.Thread):
     def __init__ (self, options, pt):
-        Thread.__init__(self)
+        threading.Thread.__init__(self)
         self._stop = threading.Event()
         self.screen = curses.initscr()
     	curses.noecho()
