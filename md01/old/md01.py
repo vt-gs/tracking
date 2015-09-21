373e680d776575f9bd619b5a6db6fac4ec55be9f
Zach Leffke <zleffke@vt.edu>
2015-09-21 03:00:26 -0400

    adding gui version of MD01 Controller

--- md01/old/md01.py
+++ md01/old/md01.py
@@ -7,17 +7,15 @@ import time
 import curses
 import threading
 from binascii import *
-from datetime import datetime as date
 from optparse import OptionParser
 
 class md01(object):
     def __init__ (self, ip, port, timeout = 1.0, retries = 2):
         self.ip         = ip        #IP Address of MD01 Controller
         self.port       = port      #Port number of MD01 Controller
-        #self.sock       = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP Socket
+        self.sock       = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP Socket
         self.timeout    = timeout   #Socket Timeout interval, default = 1.0 seconds
-        #self.sock.settimeout(timeout)   #set socket timeout
-        self.connected = False
+        self.sock.settimeout(timeout)   #set socket timeout
         self.retries    = retries   #Number of times to attempt reconnection, default = 2
         self.cmd_az     = 0         #Commanded Azimuth, used in Set Position Command
         self.cmd_el     = 0         #Commanded Elevation, used in Set Position command
@@ -33,20 +31,12 @@ class md01(object):
         for x in [0x57,0,0,0,0,0,0,0,0,0,0,0x1F,0x20]: self.status_cmd.append(x)
         for x in [0x57,0,0,0,0,0x0a,0,0,0,0,0x0a,0x2F,0x20]: self.set_cmd.append(x) #PH=PV=0x0a, 0x0a = 10, BIG-RAS/HR is 10 pulses per degree
 
-    def getTimeStampGMT(self):
-        return str(date.utcnow()) + " GMT | "
-
     def connect(self):
         #connect to md01 controller
-        self.sock       = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP Socket
-        self.sock.settimeout(self.timeout)   #set socket timeout
-        print self.getTimeStampGMT() + 'MD01 |  Attempting to connect to MD01 Controller: ' + str(self.ip) + ' ' + str(self.port)
         try:
             self.sock.connect((self.ip, self.port))
-            self.connected = True
-            return self.connected
             #upon connection, get status to determine current antenna position
-            #self.get_status()
+            self.get_status()
         except socket.error as msg:
             print "Exception Thrown: " + str(msg) + " (" + str(self.timeout) + "s)"
             print "Unable to connect to MD01 at IP: " + str(self.ip) + ", Port: " + str(self.port)  
@@ -55,11 +45,7 @@ class md01(object):
 
     def disconnect(self):
         #disconnect from md01 controller
-        print self.getTimeStampGMT() + "MD01 |  Attempting to disconnected from Tracking Controller"
-        self.sock.shutdown(socket.SHUT_RDWR)
         self.sock.close()
-        self.connected = False
-        print self.getTimeStampGMT() + "MD01 |  Successfully disconnected from Tracking Controller"
     
     def get_status(self):
         #get azimuth and elevation feedback from md01
@@ -161,3 +147,69 @@ class md01(object):
         self.set_cmd[10] = self.pv
 
 
+
+
+class Tracker_Thread(threading.Thread):
+    def __init__ (self, options):
+        threading.Thread.__init__(self)
+        self._stop = threading.Event()
+        self.ip = options.ip
+        self.port = options.port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP Socket
+        self.cmd_az = 0.0       #Commanded Azimuth
+        self.cmd_el = 0.0       #Commanded Elevation
+        self.current_az = 0.0   #Current Azimuth
+        self.current_el = 0.0   #Current Elevation
+        self.az_tol = 0.0       #Azimuth Tolerance
+        self.el_tol = 0.0       #Elevation Tolerance
+        self.status = ""        #Antenna Pedestal Status
+
+    def run(self):
+        self.sock.connect((self.ip,self.port))
+        while (not self._stop.isSet()):
+            #data = self.sock.recv(1024)
+            data = self.sock.makefile().readline(1024)
+            data = data.strip('\n')
+            data_list = data.split(',')
+            if data_list[0] == '$':
+                self.cmd_az     = float(data_list[1])   #Commanded Azimuth
+                self.cmd_el     = float(data_list[2])   #Commanded Elevation
+                self.current_az = float(data_list[3])   #Current Azimuth
+                self.current_el = float(data_list[4])   #Current Elevation
+                self.az_tol     = float(data_list[5])   #Azimuth Tolerance
+                self.el_tol     = float(data_list[6])   #Elevation Tolerance
+                self.status     = data_list[7].strip('\r')        #Antenna Pedestal Status
+            #print self.cmd_az, self.cmd_el, self.current_az, self.current_el, self.az_tol, self.el_tol, self.status
+        sys.exit()
+        #time.sleep(0.25)
+
+    
+    def Write_Message(self, az, el):
+        az_str = str(int(az))
+        el_str = str(int(el))
+
+        if len(az_str) == 1:
+            az_str = "00" + az_str
+        elif len(az_str) == 2:
+            az_str = "0" + az_str
+        
+        if len(el_str) == 1:
+            el_str = "00" + el_str
+        elif len(el_str) == 2:
+            el_str = "0" + el_str
+
+        msg = "W" + az_str + " " + el_str
+        #print msg
+        self.sock.send(msg)
+
+    def Write_Raw_Message(self, cmd):
+        self.sock.send(cmd)
+
+    def get_feedback(self):
+        return self.cmd_az, self.cmd_el, self.current_az, self.current_el, self.az_tol, self.el_tol, self.status
+
+    def stop(self):
+        self._stop.set()
+
+    def stopped(self):
+        return self._stop.isSet()
