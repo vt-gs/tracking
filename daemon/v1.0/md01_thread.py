26ca47bc847f2bd60ebd138727693baa9858f588
Zachary James Leffke <zleffke@vt.edu>
2015-12-07 12:08:54 -0500

    added timestamps to tracking daemon for accurate measurment period and thus rotation rate calculations. Ensures the az/el delta thresholds aren't exceeded by funky math.

--- daemon/v1.0/md01_thread.py
+++ daemon/v1.0/md01_thread.py
@@ -28,9 +28,11 @@ class MD01_Thread(threading.Thread):
 
         self.cur_az     = 0.0
         self.cur_el     = 0.0
+        self.cur_time   = None
 
         self.last_az    = 0.0
         self.last_el    = 0.0
+        self.last_time  = None
         self.az_thresh  = az_thresh     #Azimuth Speed threshold, for error detection, deg/s
         self.el_thresh  = el_thresh     #Elevation Speed threshold, for error detection, deg/s
 
@@ -48,6 +50,8 @@ class MD01_Thread(threading.Thread):
     def run(self):
         time.sleep(1)  #Give parent thread time to spool up
         print self.utc_ts() + self.ssid + " Thread Started..."
+        last = None
+        now = None
         while (not self._stop.isSet()):
             try:
                 if self.connected == False: 
@@ -55,17 +59,23 @@ class MD01_Thread(threading.Thread):
                     time.sleep(5)
                     if self.connected == True:
                         print self.utc_ts() + "Connected to " + self.ssid + " MD01 Controller"
+                        self.last_time = date.utcnow()
                         self.connected, self.last_az, self.last_el = self.md01.get_status()
                         #print self.last_az, self.last_el
                         time.sleep(1)
+                        
                 elif self.connected == True:
+                    self.cur_time = date.utcnow()
+                    #delta = (now - last).total_seconds
                     self.connected, self.cur_az, self.cur_el = self.md01.get_status()
                     if self.connected == False:
                         print self.utc_ts() + "Disconnected from " + self.ssid + " MD01 Controller"
                     else:
                         az_delta = abs(self.cur_az - self.last_az)
                         el_delta = abs(self.cur_el - self.last_el)
-
+                        time_delta = (self.cur_time - self.last_time).total_seconds()
+                        print self.last_time, self.cur_time, str(time_delta)
+                        print az_delta, el_delta, az_delta * (1.0/time_delta), el_delta * (1.0/time_delta)
                         if az_delta > 0: self.az_motion = True
                         else: self.az_motion = False
 
@@ -79,6 +89,7 @@ class MD01_Thread(threading.Thread):
                         else:
                             self.last_az = self.cur_az
                             self.last_el = self.cur_el
+                            self.last_time = self.cur_time
                             #print az_delta, el_delta
                         time.sleep(0.250)
             except:
