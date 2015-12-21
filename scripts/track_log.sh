c1a95e405b02f1791f1f95f0fee7430e57739435
Zach Leffke <zleffke@eddie.hume.vt.edu>
2015-12-21 23:13:35 +0000

    updated tracking log copy script

--- scripts/track_log.sh
+++ scripts/track_log.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #DATETIME=`date -u +%Y%m%d_%H%M%S.%N_UTC`
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 DATETIME=`date +%Y%m%d`
 FILENAME=$DATETIME'_tracking_daemon.log'
 echo $FILENAME
-cp /var/log/upstart/tracking_daemon.log /mnt/log/eddie/tracking/$FILENAME
+cp /var/log/upstart/tracking_daemon.log /mnt/log/tracking/$FILENAME
+chmod 666 /mnt/log/tracking/$FILENAME
